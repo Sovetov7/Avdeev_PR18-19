19e68f58c080e321a4e361b6301b3a630ab0a884
Sovetov7 <sovetov_7n@mail.ru>
2022-10-28 10:23:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/avdeev_pr18_19/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.avdeev_pr18_19
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // TODO Auto-generated method stub
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // TODO Auto-generated method stub
+        var intent: Intent? = null
+        intent = when (item.itemId) {
+            R.id.m1 -> Intent(this, GamesHistory::class.java)
+            R.id.m2 -> Intent(this, Classification::class.java)
+            R.id.m3 -> Intent(this, Components::class.java)
+            R.id.m4 -> Intent(this, Development::class.java)
+            R.id.m5 -> Intent(this, GamesAsArt::class.java)
+            R.id.m6 -> Intent(this, GamesAsLearning::class.java)
+            R.id.m7 -> Intent(this, Industry::class.java)
+            else -> return super.onOptionsItemSelected(item)
+        }
+        startActivity(intent)
+        return super.onOptionsItemSelected(item)
+    }
+}
